25adce9c36efc1995ee615c6da1e684b31cde4ab
Quan0310 <114817413+Quan0310@users.noreply.github.com>
2024-08-14 09:19:47 +0700

    update 9.19am

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -7,12 +7,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Document(collection = "products")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Product implements Serializable {
 	private static final long serialVersionUID = 3L;
 	
     @Id
+    @Schema(hidden = true) 
     private String id;
     private String name;
     private String description;
